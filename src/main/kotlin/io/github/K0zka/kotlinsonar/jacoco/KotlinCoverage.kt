6a080f30a515af85e27f949a16c1439e4499809b
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2017-01-29 13:03:51 +0100

    Need to override all delegated methods which return this.

--- src/main/kotlin/io/github/K0zka/kotlinsonar/jacoco/KotlinCoverage.kt
+++ src/main/kotlin/io/github/K0zka/kotlinsonar/jacoco/KotlinCoverage.kt
@@ -1,11 +1,23 @@
 package io.github.K0zka.kotlinsonar.jacoco
 
 import org.slf4j.LoggerFactory
+import org.sonar.api.batch.fs.InputFile
+import org.sonar.api.batch.sensor.coverage.CoverageType
 import org.sonar.api.batch.sensor.coverage.NewCoverage
 import org.sonar.api.batch.sensor.coverage.internal.DefaultCoverage
 
 class KotlinCoverage(private val delegate: DefaultCoverage) : NewCoverage by delegate {
     private val logger = LoggerFactory.getLogger(KotlinCoverage::class.java)
+
+    override fun onFile(inputFile: InputFile?): NewCoverage {
+        delegate.onFile(inputFile)
+        return this
+    }
+
+    override fun ofType(type: CoverageType?): NewCoverage {
+        delegate.ofType(type)
+        return this
+    }
 
     override fun lineHits(line: Int, hits: Int): NewCoverage {
         try {
