6e2461cd0bc0f062066eb1d3d536ddbb5b56008f
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2017-04-19 21:47:53 +0200

    Update dependencies

---
+++ src/test/kotlin/io/github/K0zka/kotlinsonar/jacoco/KotlinCoverageTest.kt
@@ -0,0 +1,31 @@
+package io.github.K0zka.kotlinsonar.jacoco
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by mirko on 22.03.17.
+ */
+class KotlinCoverageTest {
+    @Test
+    fun onFile() {
+    }
+
+    @Test
+    fun ofType() {
+    }
+
+    @Test
+    fun lineHits() {
+    }
+
+    @Test
+    fun conditions() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+}
