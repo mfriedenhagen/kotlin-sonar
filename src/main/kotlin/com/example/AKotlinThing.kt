ed9d88816f2f28ca62317dffa3eb7bf6e25cfc70
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2017-04-23 21:26:13 +0200

    Revert "Remove useless Kotlin test class"
    This reverts commit 25f7e96ac4283ea9af010e40d12355f1ebd6c84e.

---
+++ src/main/kotlin/com/example/AKotlinThing.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import java.io.PrintStream
+
+data class AKotlinThing private constructor(val name: String, val age: Int) {
+
+    fun writeIt(p: PrintStream) {
+        p.println(toString())
+    }
+
+    companion object {
+        fun valueOf(name: String, age: Int): AKotlinThing = AKotlinThing(name, age)
+    }
+}
+
+fun doIt() {
+    AKotlinThing.valueOf("Michael J.", 51).writeIt(System.out)
+}
